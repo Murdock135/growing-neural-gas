1e9337c325e2a4e4f60f9dd3ccb5d7883bee1f12
Murdock135 <particlegriffin@gmail.com>
2024-05-16 15:31:35 -0500

    animation successful

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 from src.ng_alg import NeuralGas
 from src.utils import SyntheticDataset
+import pandas as pd
+import toml
 
+# Load configuration from TOML file
+config = toml.load('config.toml')
+
+data_image = "C:/Users/Zayan/Documents/code/personal_repos/adaptive vector quantization/assets/dataset1.png"
+
+dataset = SyntheticDataset(data_image)
+dataset.generate_data_from_img()
+df_numeric_classes = dataset.transform_to_numeric_classes('class')
+df = dataset.get_dataset()
+
+print(df.head())
--- src/ng_alg.py
+++ src/ng_alg.py
@@ -1,19 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
 
 class NeuralGas:
-    def __init__(self, neurons, data, lifetime, epsilon, _lambda):
-        self.neurons = neurons
+    def __init__(self, data, neurons_n, lifetime, epsilon, _lambda, max_iter):
         self.data = data
-        self.connection_matrix = np.zeros((neurons.shape[0], neurons.shape[0]))
+        self.neurons = self.create_neurons(neurons_n)
+        self.connection_matrix = np.zeros((self.neurons.shape[0], self.neurons.shape[0]))
         self.lifetime = lifetime
         self.e = epsilon
         self._lambda = _lambda
+        self.max_iter = max_iter
 
-    def ng_one_input(self, single_input):
+    def run(self):
+        for iter in range(self.max_iter):
+            print("iter: ", iter)
+            choice = np.random.randint(0, self.data.shape[0])
+            _input = self.data[choice]
+            
+            self.ng_one_input(_input)
 
+    def ng_one_input(self, _input):
         # calculate distances
-        distances = np.linalg.norm(self.neurons-single_input, axis=1)
+        distances = np.linalg.norm(self.neurons-_input, axis=1)
 
         # sort neurons by distance from input
         neuron_idx_sorted_by_dist = np.argsort(distances) # e.g. [0.34, 0.65, 0.1] -> [2, 0, 1]
@@ -22,12 +31,20 @@ class NeuralGas:
         # make connection between two closest neurons
         self.alter_connection(neuron_idx_sorted_by_dist[0], neuron_idx_sorted_by_dist[1])
 
+        # plot after making connection
+        self.plot(_input)
+
         # update neurons
-        for idx, neuron in enumerate(self.neurons):
+        for idx, _ in enumerate(self.neurons):
+            # print("neuron = ", _)
             # find # of better neurons for each neuron
             k = np.where(neuron_idx_sorted_by_dist==idx)[0][0]
             
-            neuron = neuron + self.e * np.exp(-k/self._lambda) * (single_input - neuron)
+            # update
+            self.neurons[idx] += self.e * np.exp(-k/self._lambda) * (_input - self.neurons[idx])
+
+            # # plot after change
+            # self.plot(_input, self.neurons[idx])
 
     def alter_connection(self, r_index, c_index):
         # check if connection exists
@@ -36,9 +53,43 @@ class NeuralGas:
         else:
             self.connection_matrix[r_index, c_index] = 0
     
-    def plot(self):
-        plt.scatter(self.neurons[:,0], self.neurons[:,1], label='neurons')
-        plt.scatter(self.data[:,0], self.data[:,1], label='data')
+    def plot(self, *points):
+        fig, ax = plt.subplots()
+
+        ax.scatter(self.neurons[:,0], self.neurons[:,1], marker='X', label='neurons')
+        ax.scatter(self.data[:,0], self.data[:,1], s=0.5, label='data')
+
+        if points:
+            ax.scatter(points[0][0], points[0][1], label='current input')
+
+            if len(points) > 1:
+                ax.scatter(points[1][0], points[1][1], marker='X', label='neuron')
+
+        # find connections in connection matrix
+        for r in range(self.connection_matrix.shape[0]):
+            for c in range(self.connection_matrix.shape[1]):
+                if self.connection_matrix[r,c] > 0:
+                    neuron_a = self.neurons[r]
+                    neuron_b = self.neurons[c]
+
+                    x_coords = [neuron_a[0], neuron_b[0]]
+                    y_coords = [neuron_a[1], neuron_b[1]]
+
+                    line = mlines.Line2D(x_coords, y_coords)
+                    ax.add_line(line)                   
 
         plt.legend()
         plt.show()
+
+    def create_neurons(self, neurons_n, dist='uniform'):
+        x_min, y_min = np.amin(self.data, axis=0)
+        x_max, y_max = np.amax(self.data, axis=0)
+
+        if dist == 'uniform':
+            x_coords = np.random.uniform(x_min, x_max, neurons_n)
+            y_coords = np.random.uniform(y_min, y_max, neurons_n)
+        
+        # create else block for when dist == 'normal' later
+
+        neurons = np.column_stack((x_coords, y_coords))
+        return neurons
